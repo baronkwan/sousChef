665f905835de2e53caebbcce3eb9f0455831a54c
baronkwan <baronkwan@outlook.com>
2016-06-25 23:49:58 -0700

    build up layout

---
+++ src/index.js
@@ -0,0 +1,98 @@
+'use strict';
+
+var AlexaSkill = require('./AlexaSkill'),
+    // recipes = require('./recipes');
+
+var APP_ID = undefined; //OPTIONAL: replace with 'amzn1.echo-sdk-ams.app.[your-unique-value-here]';
+
+/**
+ * MinecraftHelper is a child of AlexaSkill.
+ * To read more about inheritance in JavaScript, see the link below.
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Introduction_to_Object-Oriented_JavaScript#Inheritance
+ */
+var sousChef = function () {
+    AlexaSkill.call(this, APP_ID);
+};
+
+// Extend AlexaSkill
+sousChef.prototype = Object.create(AlexaSkill.prototype);
+sousChef.prototype.constructor = sousChef;
+
+sousChef.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
+    var speechText = "Welcome to Amazon Alexa sousChef, you can tell me what ingredients you have in your refrigerator. See what can I do for you. Please remind that you can only name maximum 3 items.";
+    // If the user either does not reply to the welcome message or says something that is not
+    // understood, they will be prompted again with this text.
+    var repromptText = "For instructions on what you can say, please say help me.";
+    response.ask(speechText, repromptText);
+};
+
+sousChef.prototype.intentHandlers = {
+    // Custom Intent
+
+
+    "RecipeIntent": function (intent, session, response) {
+        var itemSlot = intent.slots.Item,
+            itemName;
+        if (itemSlot && itemSlot.value){
+            itemName = itemSlot.value.toLowerCase();
+        }
+
+        var cardTitle = "Recipe for " + itemName,
+            recipe = recipes[itemName],
+            speechOutput,
+            repromptOutput;
+        if (recipe) {
+            speechOutput = {
+                speech: recipe,
+                type: AlexaSkill.speechOutputType.PLAIN_TEXT
+            };
+            response.tellWithCard(speechOutput, cardTitle, recipe);
+        } else {
+            var speech;
+            if (itemName) {
+                speech = "I'm sorry, I currently do not know the recipe for " + itemName + ". What else can I help with?";
+            } else {
+                speech = "I'm sorry, I currently do not know that recipe. What else can I help with?";
+            }
+            speechOutput = {
+                speech: speech,
+                type: AlexaSkill.speechOutputType.PLAIN_TEXT
+            };
+            repromptOutput = {
+                speech: "What else can I help with?",
+                type: AlexaSkill.speechOutputType.PLAIN_TEXT
+            };
+            response.ask(speechOutput, repromptOutput);
+        }
+    },
+
+    "AMAZON.StopIntent": function (intent, session, response) {
+        var speechOutput = "Goodbye";
+        response.tell(speechOutput);
+    },
+
+    "AMAZON.CancelIntent": function (intent, session, response) {
+        var speechOutput = "Goodbye";
+        response.tell(speechOutput);
+    },
+
+    "AMAZON.HelpIntent": function (intent, session, response) {
+        var speechText = "you can tell me what ingredients you have in your refrigerator. Please remind that you can only name maximum 3 items... Now, Tell me what do you have in your refrigerator?";
+        var repromptText = "You can say things like, I want to cook but only have certain things in my refrigerator, or you can say exit... Now, what can I help you with?";
+        var speechOutput = {
+            speech: speechText,
+            type: AlexaSkill.speechOutputType.PLAIN_TEXT
+        };
+        var repromptOutput = {
+            speech: repromptText,
+            type: AlexaSkill.speechOutputType.PLAIN_TEXT
+        };
+        response.ask(speechOutput, repromptOutput);
+    }
+};
+
+exports.handler = function (event, context) {
+    var sousChef = new sousChef();
+    sousChef.execute(event, context);
+};
